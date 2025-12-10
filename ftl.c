d290f234a9408ab5b6da6d19ee1f95d4d5c46876
David <nguyentiendat.st13@nycu.edu.tw>
2025-12-10 22:06:50 +0800

    Update: finish ftl_gc

--- ftl.c
+++ ftl.c
@@ -1,12 +1,8 @@
 #include "ftl.h"
 #include "nand_read.h"
 
-#define MAX_LBA 1024
-#define UNMAPPED_PBA 0xFFFFFFFF
-#define UNMAPPED_LBA 0xFFFFFFFF
-
-uint32_t l2p_table[MAX_LBA];
-uint32_t p2l_table[BLOCK_PER_DEVICE][PAGE_PER_BLOCK];
+uint32_t l2p_table[MAX_LBA];                                    // Logical to Physical table: store physical address (PBA) of data at logical address (LBA)
+uint32_t p2l_table[BLOCK_PER_DEVICE][PAGE_PER_BLOCK];           // Physical to Logical table: store logical address (LBA) of data at physical address (PBA)
 uint32_t free_blk_idx = 0;
 uint32_t free_page_idx = 0;
 uint8_t  page_status_table[BLOCK_PER_DEVICE][PAGE_PER_BLOCK];   // To track page status: FREE, VALID, INVALID
@@ -20,7 +16,7 @@ void ftl_init()
     free_page_idx = 0;
 }
 
-int ftl_write(uint32_t lba, uint8_t *buffer)        // lba (logical block address)
+int ftl_write(uint32_t lba, uint8_t *buffer)        
 {
     if( lba >= MAX_LBA)
     {
@@ -100,22 +96,37 @@ int get_victim_block()
 
 void ftl_gc()
 {
+    uint32_t lba = 0;
     int victim_blk = get_victim_block();
     if (victim_blk == -1) 
         return;
 
-    uint8_t temp_buffer[4096]; // Giả sử page size 4KB
+    uint8_t temp_buffer[4096]; // Assume page size 4KB
 
-    // 2. Di chuyển dữ liệu Valid (Valid Page Move)
-    // ... Viết code của bạn ở đây ...
+    // Valid Page Move
+    for (int pg = 0; pg < PAGE_PER_BLOCK; pg++)
+    {
+        if (page_status_table[victim_blk][pg] == PAGE_VALID)
+        {
+            nand_read(victim_blk, pg, temp_buffer);
+            nand_program(free_blk_idx, free_page_idx, temp_buffer);
+            lba = p2l_table[victim_blk][pg];                                    // Get old Logical Address of moved page
+            p2l_table[victim_blk][pg] = UNMAPPED_LBA;                           // Reset old Logical Address to unmapped
+            p2l_table[free_blk_idx][free_page_idx] = lba;                       // Update new Logical Addess of moved page
+            l2p_table[lba] = free_blk_idx * PAGE_PER_BLOCK + free_page_idx;     // Update new Physical Address of moved page
+            page_status_table[free_blk_idx][free_page_idx] = PAGE_VALID;        // Update page status after moved
+            free_page_idx++;
+        }
+    }
 
-    // 3. Xóa Block cũ
-    // ...
+    // Delete block
+    nand_erase(victim_blk);
+    for (int pg = 0; pg < PAGE_PER_BLOCK; pg++)
+    {
+        page_status_table[victim_blk][pg] = PAGE_FREE;
+    }
 
-    // 4. Xử lý Free Block index (Logic đơn giản hóa: reset free block về đây)
+    // Logic: reset free block back to victim block for simple)
     free_blk_idx = victim_blk;
     free_page_idx = 0;
-    
-    // Lưu ý: Trong thực tế, ta cần Free List để quản lý block trống, 
-    // nhưng ở đây ta ép luôn con trỏ ghi về block vừa xóa để test cho dễ.
 }
--- ftl.h
+++ ftl.h
@@ -5,9 +5,12 @@
 #include<stdint.h>
 #include<string.h>
 
-#define PAGE_FREE    0
-#define PAGE_VALID   1
-#define PAGE_INVALID 2
+#define PAGE_FREE       0
+#define PAGE_VALID      1
+#define PAGE_INVALID    2
+#define MAX_LBA         1024                // LBA (logical block address): HOST manage
+#define UNMAPPED_PBA    0xFFFFFFFF
+#define UNMAPPED_LBA    0xFFFFFFFF
 
 void ftl_init();
 int  ftl_write(uint32_t lba, uint8_t *buffer);
